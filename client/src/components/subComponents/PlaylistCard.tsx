9ddb97b77a8dd700eb6a894ff36078efc628f4ed
GCham5 <georgeschamoun3@hotmail.com>
2023-04-01 16:24:16 -0400

    add PlaylistCard

---
+++ client/src/components/subComponents/PlaylistCard.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+
+export default function PlaylistCard({ playlist }) {
+
+  const goToPlaylist = () => {
+    window.open(playlist.url, '_blank');
+  };
+
+  return (
+    <Card>
+      <CardActionArea onClick={goToPlaylist}>
+        <CardMedia
+          component="img"
+          height="200"
+          image={playlist.image}
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {playlist.name}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
