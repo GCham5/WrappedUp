048743643ddd146d16c61a11d5b83053d9cea6c5
GCham5 <georgeschamoun3@hotmail.com>
2023-04-06 23:22:23 -0400

    add Overview component

---
+++ client/src/components/Overview.tsx
@@ -0,0 +1,130 @@
+
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Chip from '@mui/material/Chip';
+import Divider from '@mui/material/Divider';
+import WrappedInsight from './WrappedInsight';
+import PlaylistCard from './subComponents/PlaylistCard';
+
+
+export default function Overview({ playlistData }) {
+
+    const minMaxData = {
+        happiestWrapped: null,
+        saddestWrapped: null,
+        longestWrapped: null,
+        shortestWrapped: null,
+        danciestWrapped: null,
+        calmestWrapped: null,
+        basicWrapped: null,
+        uniqueWrapped: null,
+    };
+
+    // not accounting for ties
+    const findMinMax = (key: string) => {
+        let minPlaylist;
+        let maxPlaylist;
+        let minValue = Infinity;
+        let maxValue = -Infinity;
+        playlistData.forEach(playlist => {
+            if (playlist[key] > maxValue) {
+                maxValue = playlist[key];
+                maxPlaylist = playlist;
+            }
+            if (playlist[key] < minValue) {
+                minValue = playlist[key];
+                minPlaylist = playlist;
+            }
+        });
+        return { minPlaylist, maxPlaylist }
+    }
+
+    // totalDuration
+    const { minPlaylist: shortestWrapped, maxPlaylist: longestWrapped } = findMinMax('totalDuration');
+    minMaxData.shortestWrapped = shortestWrapped;
+    console.log(minMaxData.shortestWrapped)
+    minMaxData.longestWrapped = longestWrapped;
+
+    // mood
+    const { minPlaylist: saddestWrapped, maxPlaylist: happiestWrapped } = findMinMax('mood');
+    minMaxData.saddestWrapped = saddestWrapped;
+    minMaxData.happiestWrapped = happiestWrapped;
+
+    // dance
+    const { minPlaylist: calmestWrapped, maxPlaylist: danciestWrapped } = findMinMax('dance');
+    minMaxData.calmestWrapped = calmestWrapped;
+    minMaxData.danciestWrapped = danciestWrapped;
+
+    // popularity
+    const { minPlaylist: uniqueWrapped, maxPlaylist: basicWrapped } = findMinMax('popularity');
+    minMaxData.uniqueWrapped = uniqueWrapped;
+    minMaxData.basicWrapped = basicWrapped;
+
+    return (
+        <>
+            <Grid
+                container
+                direction="row"
+                justifyContent="center"
+                flexWrap='wrap'
+                alignItems='flex-end'
+                rowSpacing={5}
+                columnSpacing={5}
+            >
+                <Grid item xs={12}>
+                    <Typography variant="h4" gutterBottom>
+                        Overview Of Your Wrappeds
+                    </Typography>
+                </Grid>
+                <Grid item>
+                    <Typography variant="h6" gutterBottom>
+                        Happiest Year
+                    </Typography>
+                    <PlaylistCard playlist={minMaxData.happiestWrapped} />
+                </Grid>
+                <Grid item>
+                    <Typography variant="h6" gutterBottom>
+                        Saddest Year
+                    </Typography>
+                    <PlaylistCard playlist={minMaxData.saddestWrapped} />
+                </Grid>
+                <Grid item>
+                    <Typography variant="h6" gutterBottom>
+                        Longest Year
+                    </Typography>
+                    <PlaylistCard playlist={minMaxData.longestWrapped} />
+                </Grid>
+                <Grid item>
+                    <Typography variant="h6" gutterBottom>
+                        Shortest Year
+                    </Typography>
+                    <PlaylistCard playlist={minMaxData.shortestWrapped} />
+                </Grid>
+                <Grid item>
+                    <Typography variant="h6" gutterBottom>
+                        Danciest Year
+                    </Typography>
+                    <PlaylistCard playlist={minMaxData.danciestWrapped} />
+                </Grid>
+                <Grid item>
+                    <Typography variant="h6" gutterBottom>
+                        Calmest Year
+                    </Typography>
+                    <PlaylistCard playlist={minMaxData.calmestWrapped} />
+                </Grid>
+                <Grid item>
+                    <Typography variant="h6" gutterBottom>
+                        Most Unique Year
+                    </Typography>
+                    <PlaylistCard playlist={minMaxData.uniqueWrapped} />
+                </Grid>
+                <Grid item>
+                    <Typography variant="h6" gutterBottom>
+                        Most Basic Year
+                    </Typography>
+                    <PlaylistCard playlist={minMaxData.basicWrapped} />
+                </Grid>
+            </Grid>
+        </>
+    )
+}
